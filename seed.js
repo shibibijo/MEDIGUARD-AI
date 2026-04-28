98c9ade7798ba9efa258a1c9456ad696591f46eb
Shibi Bijo <shibibijo09085@gmail.com>
2026-04-29 00:09:34 +0530

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('./models/User');
+const User = require('./User');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/mediguard';
 
